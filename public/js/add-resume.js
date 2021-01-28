a48f0ac33dec835eb99dbc4908e6976c4bd6ffe8
Zack Shone <zackariahshone.ta@gmail.com>
2021-01-27 20:37:58 -0700

    added two more pages

---
+++ public/js/add-resume.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    
+    
+    const business = document.getElementById('business');
+    const jobTitle = document.getElementById('jobTitle');
+    const dateApplied = document.getElementById('dateApplied');
+    const jobReq = document.getElementById('jobReq');
+    const contact = document.getElementById('reqContact');
+    const submit = document.getElementById('submitButton');
+    
+    submit.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const jobInformation = {
+            'business': business.value,
+            'jobTitle': jobTitle.value,
+            'dateApplied': dateApplied.value,
+            'jobReq':jobReq.value,
+            'contact':contact.value
+        }
+        $.ajax({
+            type: "post",
+            url: "/addapp",
+            data: jobInformation,
+            dataType: "dataType",
+            success: function (res) {
+                console.log(`logs res on success ${res}`);
+            }
+        });
+        
+    })
+});
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const PORT = process.env.PORT || 8000;
+const app = express();
+// const routes = require ('./routes');
+
+// const db = require('models');
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+// app.use(routes);
+app.use(express.static('public'));
+
+// db.sequalize.sync().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`listening on port number:  ${PORT}`);
+    })
+// })
