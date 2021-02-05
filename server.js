e3b1a881484ed68baaf746e3de44b95c3c3881dc
Zack Shone <zackariahshone.ta@gmail.com>
2021-02-04 21:30:42 -0700

    grasping at strwas

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 // const PORT = process.env.PORT || 8000;
 const app = express();
-const appRoutes = require ('/routes/applications.js');
-require("/routes/htmlroutes.js")(app);
+const appRoutes = require ('./routes/applications.js');
+require("./routes/htmlroutes.js")(app);
 
 
-const db = require('/models');
+const db = require('./models');
 
 app.use(express.urlencoded({
     extended:true
 }));
 
 app.use(appRoutes);
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 db.sequelize.sync().then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
